84f64b6236c2c598c760435daab7411fed1ef28d
patriciasilva <patriciasilva@deso-se.com.br>
2022-08-12 15:55:25 -0300

    mudança no index e title

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,12 +22,17 @@ const Home: NextPage = () => {
         </Head>
         <Header classStyle={css1.header} child={
           <>
-              <Title classStyle={css2.title} text="Bem Vindos ao meu Portifólio"></Title>
+             
           </>
         }></Header>
         <Body classStyle={css4.body} child={
           <>
+              <Title classStyle={css2.title} text="Bem Vindos ao meu Portifólio,"></Title>
               <Title classStyle={css2.title} text="Me chamo Patrícia Silva Oliveira, sou web  developer"></Title>
+              <Title classStyle={css2.description} text="Lorem ipsum dolor sit amet. In omnis impedit qui facilis voluptas 33 voluptas sequi qui omnis animi. Rem soluta error aut galisum maiores ut inventore vero.
+                Ab nihil aliquid et voluptatem distinctio quo explicabo repellat nam minus ipsa et excepturi odio? Ut iusto omnis ab ducimus ullam non asperiores veritatis et iure ipsa. Sit asperiores dignissimos sed laudantium aperiam aut aliquam eius qui libero iure vel veniam similique ad harum galisum.
+                Et debitis numquam aut sunt consequuntur est totam voluptates. Et excepturi ipsa vel ducimus necessitatibus hic laborum error est quia eius. Eos sint dolorem ea molestiae quos vel suscipit ratione ut distinctio obcaecati. Id repudiandae tempore et velit alias est nobis harum ut sint voluptatem?"></Title>
+
               <ImagePhoto text='https://static.vecteezy.com/ti/fotos-gratis/p1/1256600-aguia-orgulhosa-foto.jpg' classStyle={css3.image}></ImagePhoto>
           
           </>
